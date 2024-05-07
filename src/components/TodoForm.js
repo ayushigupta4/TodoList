c7e2d42a43f7f9959ab4dafa21e9c703f0e77fb1
Ayushi Gupta <ayushi.gupta.038@gmail.com>
2024-05-07 14:29:49 +0530

    Add add, remove and edit feature

---
+++ src/components/TodoForm.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import './TodoForm.css';
+
+function TodoForm(props) {
+    const [input, setInput] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random() *10000),
+            text: input
+        });
+
+        setInput('');
+    }
+
+    const handleChange = e => {
+        setInput(e.target.value);
+    }
+
+  return (
+
+        <div className='box'>
+            <form className='todo-form' onSubmit={handleSubmit}>
+            <input type="text" placeholder="Add a Task" value={input} name="text" className='todo-input' onChange={handleChange}/>
+            <button className='todo-button'>+</button>
+
+            </form>
+        
+        </div>
+    )
+}
+
+export default TodoForm
